04b2fc5686da1f6abf319c4a43e63f00f4935eb6
Davide Corsi <davide.corsi@par-tec.it>
2021-10-01 09:42:00 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,27 @@ public class Main {
 		int m = 3;
 
 		switch(m) {
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			System.out.println("Mese di 31 giorni");
-			break;
-		case 2:
-			System.out.println("Mese di 28 giorni");
-			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			System.out.println("Mese di 30 giorni");
-			break;
-		default:
-			System.out.println("Mese non valido");
-			break;
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				System.out.println("Mese di 31 giorni");
+				break;
+			case 2:
+				System.out.println("Mese di 28 giorni");
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				System.out.println("Mese di 30 giorni");
+				break;
+			default:
+				System.out.println("Mese non valido");
+				break;
 		}
 	}
 }
